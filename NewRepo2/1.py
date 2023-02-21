c307331e9e6dac1a832791acefce8506dfd76570
Eric Low <64390777+just-eric-99@users.noreply.github.com>
2023-02-21 22:50:07 +0800

    Add files via upload

---
+++ NewRepo2/1.py
@@ -0,0 +1,45 @@
+# create a fibonnaci function and example
+
+def fibonacci(n): 
+    a = 0
+    b = 1
+    if n < 0: 
+        print("Incorrect input") 
+    elif n == 0: 
+        return a 
+    elif n == 1: 
+        return b 
+    else: 
+        for i in range(2,n): 
+            c = a + b 
+            a = b 
+            b = c 
+        return b 
+  
+# Driver Program 
+print(fibonacci(9)) 
+
+# Output: 34
+
+# create one with iterative method
+
+def fibonacci_iterative(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+# Driver Program
+print(fibonacci_iterative(9))
+
+# Output: 34
